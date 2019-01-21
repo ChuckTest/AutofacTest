71dd19191d93f64cd6032c559bb7a6a83c7d57d3
Chuck Lu <chuck.lu@qq.com>
2019-01-21 18:03:28 +0800

    register service and resolve it later for usage

--- AutofacTest/Program.cs
+++ AutofacTest/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Autofac;
 
 namespace AutofacTest
 {   // This interface helps decouple the concept of
@@ -59,8 +56,32 @@ namespace AutofacTest
 
     class Program
     {
+        private static IContainer Container { get; set; }
+
         static void Main(string[] args)
         {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleOutput>().As<IOutput>();
+            builder.RegisterType<TodayWriter>().As<IDateWriter>();
+            Container = builder.Build();
+
+            // The WriteDate method is where we'll make use
+            // of our dependency injection. We'll define that
+            // in a bit.
+            WriteDate();
+
+            Console.ReadLine();
+        }
+
+        public static void WriteDate()
+        {
+            // Create the scope, resolve your IDateWriter,
+            // use it, then dispose of the scope.
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var writer = scope.Resolve<IDateWriter>();
+                writer.WriteDate();
+            }
         }
     }
 }
