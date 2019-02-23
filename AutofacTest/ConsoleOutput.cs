d1680f9fa5b83ac222bdd3a1aea154982ca5d42b
Chuck Lu <chuck.lu@qq.com>
2019-02-23 13:47:30 +0800

    split the classes and interfaces to independent files

---
+++ AutofacTest/ConsoleOutput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AutofacTest
+{
+    /// <summary>
+    /// This implementation of the IOutput interface
+    /// is actually how we write to the Console. Technically
+    /// we could also implement IOutput to write to Debug
+    /// or Trace... or anywhere else.
+    /// </summary>
+    public class ConsoleOutput : IOutput
+    {
+        public void Write(string content)
+        {
+            Console.WriteLine(content);
+        }
+    }
+}
---
+++ AutofacTest/IOutput.cs
@@ -0,0 +1,13 @@
+﻿namespace AutofacTest
+{
+    /// <summary>
+    /// This interface helps decouple the concept of
+    /// "writing output" from the Console class. We
+    /// don't really "care" how the Write operation
+    /// happens, just that we can write.
+    /// </summary>
+    public interface IOutput
+    {
+        void Write(string content);
+    }
+}
