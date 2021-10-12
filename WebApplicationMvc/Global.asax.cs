d3a15d0eb33faec70ce2b2751406c67965bf24ed
Chuck LU <chucklu2015@outlook.com>
2021-10-12 17:48:20 +0800

    register ioc for mvc https://autofac.readthedocs.io/en/latest/integration/mvc.html#quick-start

--- WebApplicationMvc/Global.asax.cs
+++ WebApplicationMvc/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
 
 namespace WebApplicationMvc
 {
@@ -16,6 +18,19 @@ namespace WebApplicationMvc
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            RegisterIoc();
+        }
+
+        private void RegisterIoc()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
     }
 }
