6c1938dda6e812adb4b8bd5fffc187bbdca6f589
Chuck LU <chucklu2015@outlook.com>
2021-10-12 17:40:32 +0800

    add model and validation helper

---
+++ WebApplicationMvc/Helper/Validation/ValidationHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplicationMvc.Models;
+
+namespace WebApplicationMvc.Helper.Validation
+{
+    public class ValidationHelperBase<T> where T : CustomTableBase
+    {
+        protected virtual void CheckExist(int customTableItemId)
+        {
+           //do logic check, and throw exception when check failed
+        }
+    }
+}
---
+++ WebApplicationMvc/Models/CustomTableBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationMvc.Models
+{
+    public class CustomTableBase
+    {
+        public int CustomTableItemId { get; set; }
+    }
+}
